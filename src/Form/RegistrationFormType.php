c6d3694fe1992570877a06d2db0790a79b968f4b
salut <killian.portier@yahoo.fr>
2023-05-10 11:13:22 +0200

    wip for the style of form wait to see with the masters

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,10 @@ namespace App\Form;
 use App\Entity\Entreprise;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,31 +20,69 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
+            ->add('email', EmailType::class, [
+                'label' => 'Veuillez saisir l\'adresse mail de votre entreprise :',
                 'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+                    new NotBlank([
+                        'message' => 'Vous devez saisir une adresse mail'
+                    ])
                 ],
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-control'
+                ]
             ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('name', TextType::class, [
+                'label' => 'Le nom de votre entreprise :',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir le nom de votre entreprise'
+                    ])
+                ],
+                'required' => false,
+                'attr' => [
+                    'class' => 'qsa-input-form rounded'
+                ]
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'required' => false,
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les deux mot de passe doivent être identiques',
+                'first_name' => 'first',
+                'second_name' => 'second',
+                'first_options' => [
+
+                    'label' => 'Veuillez saisir un mot de passe :',
+                    'attr' => [
+                        'class' => 'd-flex form-control'
+                    ],
+
+                ],
+                'second_options' => [
+                    'label' => 'Veuillez resaisir votre mot de passe :',
+                    'label_attr' => [
+                        'class' => 'mt-1',
+                    ],
+                    'attr' => [
+                        'class' => 'form-control',
+                    ],
+                ],
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Vous devez saisir un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit faire au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
+
             ])
         ;
     }
