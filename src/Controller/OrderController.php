410839a471295d39301a0e57abeed49c653b327d
salut <killian.portier@yahoo.fr>
2023-05-15 12:01:35 +0200

    fix

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -12,7 +12,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use function Sodium\add;
 
 class OrderController extends AbstractController
 {
